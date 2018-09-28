36de30e9be1fec49d9593f9efa1c482aa28cf3a1
Roman <gambro87@gmail.com>
2018-09-28 23:40:44 +0300

    fix deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,12 +9,12 @@ set :puma_workers, 0
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :deploy_to,       "/home/#{fetch(:user)}#{fetch(:application)}"
+set :puma_bind,       "unix://#{fetch(:application)}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{fetch(:application)}/tmp/pids/puma.state"
+set :puma_pid,        "#{fetch(:application)}/tmp/pids/puma.pid"
+set :puma_access_log, "#{fetch(:application)}/log/puma.error.log"
+set :puma_error_log,  "#{fetch(:application)}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
@@ -25,8 +25,8 @@ namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
   task :make_dirs do
     on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
+      execute "mkdir #{fetch(:application)}/tmp/sockets -p"
+      execute "mkdir #{fetch(:application)}/tmp/pids -p"
     end
   end
 
@@ -83,10 +83,10 @@ end
 set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/secrets.yml"
+# append :linked_files, "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
