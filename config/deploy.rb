3561ef72e743d5a83159a9bb3fc0b412152d53c0
Roman <gambro87@gmail.com>
2018-09-28 23:11:13 +0300

    fix deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,6 +19,7 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+set :branch, "master"
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
